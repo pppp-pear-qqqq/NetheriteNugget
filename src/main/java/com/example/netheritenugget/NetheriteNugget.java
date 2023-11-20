0179223aad8a7c9702d3524924e5bc9de5382b57
21010016ni <105106328+21010016ni@users.noreply.github.com>
2023-11-20 20:10:08 +0900

    いい感じ
    多分できました

--- src/main/java/com/example/netheritenugget/NetheriteNugget.java
+++ src/main/java/com/example/netheritenugget/NetheriteNugget.java
@@ -44,7 +44,6 @@ public class NetheriteNugget
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::registerProviders);
 
         // Register the Deferred Register to the mod event bus so items get registered
@@ -55,22 +54,6 @@ public class NetheriteNugget
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // Add the example block item to the building blocks tab
